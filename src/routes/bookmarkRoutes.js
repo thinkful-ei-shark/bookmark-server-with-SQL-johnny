3772e542408522dd93ea531aa4b4ae6536a3d872
Dominic Volante <dominicvolante@gmail.com>
2020-10-23 15:11:16 -0400

    implemented POST route

--- src/routes/bookmarkRoutes.js
+++ src/routes/bookmarkRoutes.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = require('../app');
 const logger = require('../logger');
+const bodyParser = express.json();
+const { uuid } = require('uuidv4');
+const validUrl = require('valid-url');
 
 const bookmarkRouter = express.Router();
 
 const { bookmarks } = require('../store');
+const store = require('../store');
 
 bookmarkRouter.get('/', (req, res) => {
   res.json(bookmarks);
@@ -19,5 +23,27 @@ bookmarkRouter.get('/:id', (req, res) => {
   }
   res.json(foundBookmark);
 });
+
+
+bookmarkRouter.post('/', bodyParser, (req, res)=>{
+  const { title, url, description, rating } = req.body
+  if(!title || !url || !description || !rating){
+    logger.error('Must have valid title, url, description, and rating')
+    return res.status(400).send('All fields required')
+  }
+  const bookmark = { id: uuid(), title, url, description, rating}
+
+  if(!validUrl.isUri(url)){
+    logger.error('Invalid URL format')
+    return res.status(400).send(`${url} is not a valid URL format`)
+  }
+  if(typeof rating !== 'number'){
+    logger.error('Rating is not a number')
+    return res.status(400).send(`${rating} is not a number`)
+  }
+  
+  store.bookmarks.push(bookmark)
+  res.status(201).json(bookmark)
+})
 
 module.exports = bookmarkRouter;
